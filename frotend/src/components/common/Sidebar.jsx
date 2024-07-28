069ec06d8c639181234231d317ac0e2fe37d0063
sagar <sagartup20@gmail.com>
2024-07-28 23:27:15 +0530

    add

--- frotend/src/components/common/Sidebar.jsx
+++ frotend/src/components/common/Sidebar.jsx
@@ -5,50 +5,34 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
-import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
-
-
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
-
-	const queryClient =  useQueryClient();
-	const {mutate:logout, isPending, isError, error} = useMutation({
+	const queryClient = useQueryClient();
+	const { mutate: logout } = useMutation({
 		mutationFn: async () => {
 			try {
 				const res = await fetch("/api/auth/logout", {
 					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-					},
 				});
 				const data = await res.json();
-				if (!res.ok) throw new Error(data.error || "Failed to logout");
-				return data;
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
 			} catch (error) {
-				console.error(error);
-				throw error;
+				throw new Error(error);
 			}
 		},
 		onSuccess: () => {
 			queryClient.invalidateQueries({ queryKey: ["authUser"] });
 		},
+		onError: () => {
+			toast.error("Logout failed");
+		},
 	});
-	
-	const handleLogout = () => {
-		mutate();
-	};
-	
-	if(isError){
-		console.log(error.message);
-		
-	}
-	
-	const data = {
-		fullName: "John Doe",
-		username: "johndoe",
-		profileImg: "/avatar/boy1.png",
-	};
-	const {data:authuser} = useQuery({queryKey:['authuser']});
+	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
 
 	return (
 		<div className='md:flex-[2_2_0] w-18 max-w-52'>
@@ -78,7 +62,7 @@ const Sidebar = () => {
 
 					<li className='flex justify-center md:justify-start'>
 						<Link
-							to={`/profile/${data?.username}`}
+							to={`/profile/${authUser?.username}`}
 							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
 							<FaUser className='w-6 h-6' />
@@ -86,26 +70,27 @@ const Sidebar = () => {
 						</Link>
 					</li>
 				</ul>
-				{data && (
+				{authUser && (
 					<Link
-						to={`/profile/${data.username}`}
+						to={`/profile/${authUser.username}`}
 						className='mt-auto mb-10 flex gap-2 items-start transition-all duration-300 hover:bg-[#181818] py-2 px-4 rounded-full'
 					>
 						<div className='avatar hidden md:inline-flex'>
 							<div className='w-8 rounded-full'>
-								<img src={data?.profileImg || "/avatar-placeholder.png"} />
+								<img src={authUser?.profileImg || "/avatar-placeholder.png"} />
 							</div>
 						</div>
 						<div className='flex justify-between flex-1'>
 							<div className='hidden md:block'>
-								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
-								<p className='text-slate-500 text-sm'>@{data?.username}</p>
+								<p className='text-white font-bold text-sm w-20 truncate'>{authUser?.fullName}</p>
+								<p className='text-slate-500 text-sm'>@{authUser?.username}</p>
 							</div>
-							<BiLogOut className='w-5 h-5 cursor-pointer'
-							onClick={(e)=>{
-								e.preventDefault();
-								logout();
-							}}
+							<BiLogOut
+								className='w-5 h-5 cursor-pointer'
+								onClick={(e) => {
+									e.preventDefault();
+									logout();
+								}}
 							/>
 						</div>
 					</Link>
